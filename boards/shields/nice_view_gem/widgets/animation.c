ce69e69de3ced987e1c5a9481312c73484057732
chyuuu <bernice.simon01@gmail.com>
2026-06-29 18:56:08 -0400

    rotate image

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -11,5 +11,6 @@ LV_IMG_DECLARE(custom_logo);
 void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_img_create(canvas);
     lv_img_set_src(art, &custom_logo);
+    lv_img_set_angle(art, 900); 
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
 }
