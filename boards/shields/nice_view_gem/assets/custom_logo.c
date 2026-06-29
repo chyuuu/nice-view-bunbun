65d17980d40f5260afeae3a079e2732a4ddaa7c3
chyuuu <bernice.simon01@gmail.com>
2026-06-29 15:43:09 -0400

    Update custom_logo.c

--- boards/shields/nice_view_gem/assets/custom_logo.c
+++ boards/shields/nice_view_gem/assets/custom_logo.c
@@ -22,13 +22,8 @@
 #endif
 
 const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_CUSTOM_LOGO uint8_t custom_logo_map[] = {
-#if CONFIG_NICE_VIEW_WIDGET_INVERTED
-        0x00, 0x00, 0x00, 0xff, /*Color of index 0*/
-        0xff, 0xff, 0xff, 0xff, /*Color of index 1*/
-#else
-        0xff, 0xff, 0xff, 0xff, /*Color of index 0*/
-        0x00, 0x00, 0x00, 0xff, /*Color of index 1*/
-#endif
+  0xff, 0xff, 0xff, 0xff, 	/*Color of index 0*/
+  0x00, 0x00, 0x00, 0x74, 	/*Color of index 1*/
 
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf8, 
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf8, 
@@ -102,6 +97,8 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_CUSTOM_LO
 
 const lv_img_dsc_t custom_logo = {
   .header.cf = LV_COLOR_FORMAT_I1,
+  .header.always_zero = 0,
+  .header.reserved = 0,
   .header.w = 69,
   .header.h = 68,
   .data_size = 620,
